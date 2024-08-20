c7c808bd2b7cc4a642e16a43d568d763a0859b37
한채연 <166791875+hanchaeyeon7588@users.noreply.github.com>
2024-08-20 12:07:58 +0900

    페이지 이름 변경
    마이도감에서 다이어리로 변경

--- wonroom/lib/myPlant.dart
+++ wonroom/lib/myPlant.dart
@@ -8,7 +8,7 @@ class Myplant extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('마이 도감', style: TextStyle(color: Colors.black)),
+        title: Text('다이어리', style: TextStyle(color: Colors.black)),
         backgroundColor: Colors.white,
         elevation: 0,
         leading: IconButton(
