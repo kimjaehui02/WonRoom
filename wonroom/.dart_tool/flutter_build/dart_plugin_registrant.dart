77363b8ecea56671e865ea5d657fcca976333964
kimjaehui02 <momg4287@naver.com>
2024-08-22 17:02:18 +0900

    뭔진모르겠지만 됨

--- wonroom/.dart_tool/flutter_build/dart_plugin_registrant.dart
+++ wonroom/.dart_tool/flutter_build/dart_plugin_registrant.dart
@@ -6,11 +6,22 @@
 // @dart = 3.4
 
 import 'dart:io'; // flutter_ignore: dart_io_import.
+import 'package:image_picker_android/image_picker_android.dart';
 import 'package:path_provider_android/path_provider_android.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:image_picker_ios/image_picker_ios.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:file_selector_linux/file_selector_linux.dart';
+import 'package:image_picker_linux/image_picker_linux.dart';
 import 'package:path_provider_linux/path_provider_linux.dart';
+import 'package:file_selector_macos/file_selector_macos.dart';
+import 'package:image_picker_macos/image_picker_macos.dart';
 import 'package:path_provider_foundation/path_provider_foundation.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:file_selector_windows/file_selector_windows.dart';
 import 'package:flutter_secure_storage_windows/flutter_secure_storage_windows.dart';
+import 'package:image_picker_windows/image_picker_windows.dart';
 import 'package:path_provider_windows/path_provider_windows.dart';
 
 @pragma('vm:entry-point')
@@ -20,6 +31,15 @@ class _PluginRegistrant {
   static void register() {
     if (Platform.isAndroid) {
       try {
+        ImagePickerAndroid.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_android` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderAndroid.registerWith();
       } catch (err) {
         print(
@@ -28,7 +48,25 @@ class _PluginRegistrant {
         );
       }
 
+      try {
+        SqflitePlugin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isIOS) {
+      try {
+        ImagePickerIOS.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_ios` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PathProviderFoundation.registerWith();
       } catch (err) {
@@ -38,7 +76,34 @@ class _PluginRegistrant {
         );
       }
 
+      try {
+        SqflitePlugin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isLinux) {
+      try {
+        FileSelectorLinux.registerWith();
+      } catch (err) {
+        print(
+          '`file_selector_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        ImagePickerLinux.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_linux` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         PathProviderLinux.registerWith();
       } catch (err) {
@@ -50,6 +115,24 @@ class _PluginRegistrant {
 
     } else if (Platform.isMacOS) {
       try {
+        FileSelectorMacOS.registerWith();
+      } catch (err) {
+        print(
+          '`file_selector_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        ImagePickerMacOS.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_macos` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
         PathProviderFoundation.registerWith();
       } catch (err) {
         print(
@@ -58,12 +141,39 @@ class _PluginRegistrant {
         );
       }
 
+      try {
+        SqflitePlugin.registerWith();
+      } catch (err) {
+        print(
+          '`sqflite` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
     } else if (Platform.isWindows) {
+      try {
+        FileSelectorWindows.registerWith();
+      } catch (err) {
+        print(
+          '`file_selector_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
       try {
         FlutterSecureStorageWindows.registerWith();
       } catch (err) {
         print(
           '`flutter_secure_storage_windows` threw an error: $err. '
+          'The app may not function as expected until you remove this plugin from pubspec.yaml'
+        );
+      }
+
+      try {
+        ImagePickerWindows.registerWith();
+      } catch (err) {
+        print(
+          '`image_picker_windows` threw an error: $err. '
           'The app may not function as expected until you remove this plugin from pubspec.yaml'
         );
       }
