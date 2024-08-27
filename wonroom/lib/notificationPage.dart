8292b2f6d05a47925260ae04977e11b59336670d
한채연 <166791875+hanchaeyeon7588@users.noreply.github.com>
2024-08-27 10:18:39 +0900

    알림 목록
    알림 목록

--- wonroom/lib/notificationPage.dart
+++ wonroom/lib/notificationPage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
+
 class NotificationPage extends StatelessWidget {
   const NotificationPage({super.key});
 
@@ -22,7 +23,6 @@ class NotificationPage extends StatelessWidget {
         ),
       ),
       body: Container(
-        color: Colors.white,
         child: Column(
           children: [
             SizedBox(height: 3,),
@@ -55,26 +55,81 @@ class NotificationPage extends StatelessWidget {
               ),
             ),
 
-            Container(
-              padding: EdgeInsets.all(16),
-              child: Row(
+            SizedBox(height: 10,),
+
+            // 내역
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
                 children: [
-                  Image.asset('images/')
-                  Text(
-                    '커뮤니티',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
+                  Container(
+                    padding: EdgeInsets.all(16),
+                    margin: EdgeInsets.only(bottom: 10),
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
+                      color: Color(0xfff7f7f7),
                     ),
-                  ),
-                  SizedBox(width: 10),
-                  Icon(Icons.access_time, size: 14, color: Colors.grey),
-                  SizedBox(width: 2),
-                  Text(
-                    '치코님이 원룸님 게시글에 댓글을 남겼어요.',
-                    style: TextStyle(
-                      color: Colors.grey,
-                      fontSize: 14,
+                    child: Row(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.12,
+                          height: MediaQuery.of(context).size.width * 0.12,
+                          decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular(50),
+                            border: Border.all(
+                              color: Color(0xffc2c2c2),
+                              width: 1,
+                            ),
+                            image: DecorationImage(
+                              image: AssetImage('images/plant_0.jpg'),
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+
+                        SizedBox(width: 10),
+
+                        Expanded(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                children: [
+                                  Text(
+                                    '커뮤니티',
+                                    style: TextStyle(
+                                      color: Color(0xff86b26a),
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  Text(
+                                    '2일전',
+                                    style: TextStyle(
+                                      color: Colors.grey,
+                                      fontSize: 14,
+                                    ),
+                                  ),
+                                ],
+                              ),
+
+                              SizedBox(height: 2),
+
+                              Text(
+                                '치코님이 원룸님 게시글에 댓글을 남겼어요.',
+                                style: TextStyle(
+                                  color: Color(0xff595959),
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
