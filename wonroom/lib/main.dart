677d19499ec97f4edeb909abfeb9f60ca9c8da2c
전다혜 <166770562+jeonnda@users.noreply.github.com>
2024-08-28 18:08:38 +0900

    쟈병합

--- wonroom/lib/main.dart
+++ wonroom/lib/main.dart
@@ -47,7 +47,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.white, // 앱바 배경색
       ),
     ),
-    home: Community(),
+    home: MyPost(),
     );
   }
 }
---
+++ wonroom/lib/writingInquiries.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class WritingInquiries extends StatelessWidget {
+  const WritingInquiries({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+}
