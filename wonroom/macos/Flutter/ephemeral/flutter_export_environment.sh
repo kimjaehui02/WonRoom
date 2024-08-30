fb32594da28f0b9f2510165ed7bcdc86d52eb054
kimjaehui02 <momg4287@naver.com>
2024-08-30 11:14:39 +0900

    병합이 이상함

--- wonroom/macos/Flutter/ephemeral/flutter_export_environment.sh
+++ wonroom/macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\Users\smhrd\Desktop\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\Users\smhrd\Desktop\project\WonRoom\wonroom"
+export "FLUTTER_ROOT=C:\Users\smhrd1\Desktop\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\smhrd1\Desktop\thirdproject\WonRoom\wonroom"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
