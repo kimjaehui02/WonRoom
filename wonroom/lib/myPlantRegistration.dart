13520716cb40feedd88628262a1bc98cf122d6a0
한채연 <166791875+hanchaeyeon7588@users.noreply.github.com>
2024-08-30 18:34:10 +0900

    다이어리 등록 임시 저장
    다이어리 등록 임시 저장

--- wonroom/lib/myPlantRegistration.dart
+++ wonroom/lib/myPlantRegistration.dart
@@ -36,7 +36,7 @@ void showPlantRegistrationModal(BuildContext context) {
                             ),
                           ),
                         ),
-                        SizedBox(height: 16),
+                        SizedBox(height: 20),
                         Center(
                           child: Text(
                             '식물 등록',
@@ -51,38 +51,51 @@ void showPlantRegistrationModal(BuildContext context) {
                           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                           children: [
                             _buildImageButton(
-                              icon: Icons.camera_alt,
-                              label: '사진촬영',
+                              icon: Icons.camera_alt_outlined,
+                              label: '사진 촬영',
                               onPressed: () {},
                             ),
+
+                            Container(
+                              width: 1,
+                              height: 50,
+                              color: Color(0xffc2c2c2),
+                            ),
+
                             _buildImageButton(
-                              icon: Icons.photo_library,
+                              icon: Icons.photo_library_outlined,
                               label: '앨범에서 선택',
                               onPressed: () {},
                             ),
                           ],
                         ),
+
                         SizedBox(height: 20),
+
                         Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          padding: const EdgeInsets.all(16),
                           child: _buildTextField(label: '식물 이름', hintText: '파키라'),
                         ),
-                        SizedBox(height: 20),
+                        SizedBox(height: 10),
+
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 16.0),
                           child: _buildDatePicker(context, label: '마지막 물준날'),
                         ),
-                        SizedBox(height: 20),
+                        SizedBox(height: 24),
+
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 16.0),
                           child: _buildDatePicker(context, label: '마지막 영양제'),
                         ),
-                        SizedBox(height: 20),
+                        SizedBox(height: 24),
+
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 16.0),
                           child: _buildDatePicker(context, label: '마지막 분갈이'),
                         ),
                         SizedBox(height: 24),
+
                         Padding(
                           padding: const EdgeInsets.symmetric(horizontal: 16.0),
                           child: _buildDatePicker(context, label: '마지막 가지치기'),
@@ -149,21 +162,21 @@ Widget _buildImageButton({
     child: Container(
       decoration: BoxDecoration(
         border: Border(
-          top: BorderSide(color: Colors.grey),     // 상단에 회색 선
-          bottom: BorderSide(color: Colors.grey),  // 하단에 회색 선
-          left: BorderSide(color: Colors.grey),    // 좌측에 회색 선
-          right: BorderSide(color: Colors.grey),   // 우측에 회색 선
+          top: BorderSide(color: Color(0xffc2c2c2)),
+          bottom: BorderSide(color: Color(0xffc2c2c2)),
+          // left: BorderSide(color: Colors.grey),
+          // right: BorderSide(color: Colors.grey),
         ),
       ),
       child: TextButton.icon(
         onPressed: onPressed,
-        icon: Icon(icon, color: Colors.grey),
+        icon: Icon(icon, color: Color(0xff787878)),
         label: Text(
           label,
-          style: TextStyle(color: Colors.grey),
+          style: TextStyle(color: Color(0xff333333)),
         ),
         style: TextButton.styleFrom(
-          padding: EdgeInsets.symmetric(vertical: 16), // 버튼 높이를 조절
+          padding: EdgeInsets.symmetric(vertical: 10),
         ),
       ),
     ),
@@ -174,14 +187,19 @@ Widget _buildTextField({required String label, required String hintText}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text(
-        label,
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      Padding(
+        padding: const EdgeInsets.only(left: 6),
+        child: Text(
+          label,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
       ),
+      SizedBox(height: 10),
       TextField(
         decoration: InputDecoration(
           hintText: hintText,
           border: OutlineInputBorder(),
+          contentPadding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
         ),
       ),
     ],
@@ -194,10 +212,14 @@ Widget _buildDatePicker(BuildContext context, {required String label}) {
   return Column(
     crossAxisAlignment: CrossAxisAlignment.start,
     children: [
-      Text(
-        label,
-        style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+      Padding(
+        padding: const EdgeInsets.only(left: 6),
+        child: Text(
+          label,
+          style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
       ),
+      SizedBox(height: 10),
       GestureDetector(
         onTap: () async {
           DateTime? pickedDate = await showDatePicker(
@@ -216,6 +238,7 @@ Widget _buildDatePicker(BuildContext context, {required String label}) {
             decoration: InputDecoration(
               hintText: '날짜 선택',
               border: OutlineInputBorder(),
+              contentPadding: EdgeInsets.symmetric(horizontal: 12.0, vertical: 10.0),
             ),
           ),
         ),
